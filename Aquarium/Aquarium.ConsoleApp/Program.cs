e167577dfbd95e566b3a3010af986aab881a2a69
nodeJayS <shinjayw@gmail.com>
2020-11-16 20:31:34 -0500

    More try catch blocks added

--- Aquarium/Aquarium.ConsoleApp/Program.cs
+++ Aquarium/Aquarium.ConsoleApp/Program.cs
@@ -90,13 +90,21 @@ namespace Aquarium.ConsoleApp
                     Console.WriteLine($"Updating current store inventory. Here is a list of all animals and their quantity at store location ({store.City}). Please type name of animal.");
                     store.GetStoreInventory();
                     input = Console.ReadLine();
-                    var CurrentAnimal = Current.GetAnimalByName(input);
-                    Console.WriteLine($"How many {CurrentAnimal}s would you like to insert?");
-                    var input2 = Console.ReadLine();
-                    var quantityInt = Int32.Parse(input2);
-                    store.AddToInventory(CurrentAnimal, quantityInt);
-                    Current.UpdateInventory(store.City, CurrentAnimal, quantityInt);
-                    Console.WriteLine($"{CurrentAnimal.Name} with a quantity of {input2} has been inserted.");
+                    try
+                    {
+                        var CurrentAnimal = Current.GetAnimalByName(input);
+                        Console.WriteLine($"How many {CurrentAnimal}s would you like to insert?");
+                        var input2 = Console.ReadLine();
+                        var quantityInt = Int32.Parse(input2);
+                        store.AddToInventory(CurrentAnimal, quantityInt);
+                        Current.UpdateInventory(store.City, CurrentAnimal, quantityInt);
+                        Console.WriteLine($"{CurrentAnimal.Name} with a quantity of {input2} has been inserted.");
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine($"Animal {input} was not found in our database.");
+                        StoreInput(store);
+                    }
                     break;
                 case "3":
                     Console.WriteLine($"Searching for all orders made at store location ({store.City}).");
@@ -126,8 +134,16 @@ namespace Aquarium.ConsoleApp
                 case "1":
                     Console.WriteLine("Find customer by name. Please input the customer's email");
                     input = Console.ReadLine();
-                    var ExistingCust = Current.GetCustomerByEmail(input);
-                    ExistingCust.GetCustomerInfo();
+                    try
+                    {
+                        var ExistingCust = Current.GetCustomerByEmail(input);
+                        ExistingCust.GetCustomerInfo();
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine($"Email input {input} was not found in our database. Please create a new customer.");
+                        CustomerInput(store);
+                    }
                     break;
                 case "2":
                     Console.WriteLine("Create a new customer. Please input new customer's first name");
@@ -147,11 +163,18 @@ namespace Aquarium.ConsoleApp
                 case "3":
                     Console.WriteLine("Modify an existing customer. Please input the customer's email");
                     input = Console.ReadLine();
-                    var CurrentCust = Current.GetCustomerByEmail(input);
-                    Console.WriteLine("Customer found. Input customer's new email");
-                    CurrentCust.Email = Console.ReadLine();
-                    Current.UpdateCustomer(CurrentCust);
-                    Console.WriteLine($"New Email: {CurrentCust.Email} has been saved.");
+                    try
+                    {
+                        var CurrentCust = Current.GetCustomerByEmail(input);
+                        Console.WriteLine("Customer found. Input customer's new email");
+                        CurrentCust.Email = Console.ReadLine();
+                        Current.UpdateCustomer(CurrentCust);
+                        Console.WriteLine($"New Email: {CurrentCust.Email} has been saved.");
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine($"Could not find email.");
+                    }
                     break;
                 default:
                     Console.WriteLine("Please try again.");
@@ -200,40 +223,47 @@ namespace Aquarium.ConsoleApp
                 case "3":
                     Console.WriteLine($"Creating a new order in store location ({store.City}). Please input customer's email");
                     input = Console.ReadLine();
-                    var NewOrder = new Library.Order();
-                    NewOrder.Customer = Current.GetCustomerByEmail(input); // Need to check to see if email exists
-                    Console.WriteLine($"Input name of the animal for this purchase. Our store location at ({store.City}) has:");
-                    store.GetStoreInventory();
-                    var animalNameInput = Console.ReadLine(); // Need to check if animal name exists. If it does, continue
-                    NewOrder.Animal = Current.GetAnimalByName(animalNameInput);
-                    Console.WriteLine($"How many {animalNameInput}s for purchase?");
-                    var QuantityInput = Console.ReadLine(); // Need to check if store has enough animals in inventory
-                    var Quant = Int32.Parse(QuantityInput);
-                    if (store.InInventory(NewOrder.Animal)) {
-                        if(store.RemoveFromInventory(NewOrder.Animal, Quant))
+                    try
+                    {
+                        var NewOrder = new Library.Order();
+                        NewOrder.Customer = Current.GetCustomerByEmail(input); // Need to check to see if email exists
+                        Console.WriteLine($"Input name of the animal for this purchase. Our store location at ({store.City}) has:");
+                        store.GetStoreInventory();
+                        var animalNameInput = Console.ReadLine(); // Need to check if animal name exists. If it does, continue
+                        NewOrder.Animal = Current.GetAnimalByName(animalNameInput);
+                        Console.WriteLine($"How many {animalNameInput}s for purchase?");
+                        var QuantityInput = Console.ReadLine(); // Need to check if store has enough animals in inventory
+                        var Quant = Int32.Parse(QuantityInput);
+                        if (store.InInventory(NewOrder.Animal))
                         {
-                            NewOrder.Quantity = Quant;
-                            NewOrder.StoreId = store.StoreId;
-                            NewOrder.Date = DateTime.Now;
-                            NewOrder.GetTotal();
-                            Console.WriteLine("Order created. Order receipt:");
-                            Current.CreateOrder(NewOrder);
-                            NewOrder.GetOrderInfo();
-                            break;
+                            if (store.RemoveFromInventory(NewOrder.Animal, Quant))
+                            {
+                                NewOrder.Quantity = Quant;
+                                NewOrder.StoreId = store.StoreId;
+                                NewOrder.Date = DateTime.Now;
+                                NewOrder.GetTotal();
+                                Console.WriteLine("Order created. Order receipt:");
+                                Current.CreateOrder(NewOrder);
+                                NewOrder.GetOrderInfo();
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Not enough {NewOrder.Animal.Name} in stock.");
+                                OrderInput(store);
+                            }
                         }
                         else
                         {
-                            Console.WriteLine($"Not enough {NewOrder.Animal.Name} in stock.");
+                            Console.WriteLine($"{NewOrder.Animal.Name} not found in inventory.");
                             OrderInput(store);
-                            break;
-                        }
+                        };
                     }
-                    else
+                    catch (NullReferenceException)
                     {
-                        Console.WriteLine($"{NewOrder.Animal.Name} not found in inventory.");
+                        Console.WriteLine("Error");
                         OrderInput(store);
-                        break;
                     }
+                    break;
                 default:
                     Console.WriteLine("Please try again.");
                     OrderInput(store);
